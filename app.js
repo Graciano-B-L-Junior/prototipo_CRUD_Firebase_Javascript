453286ec0f3fed95e15e294b283579e47e62f7f7
Graciano Junior <gracianobarbosalimajunior@gmail.com>
2021-02-24 15:29:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const submit = document.getElementById('submit');
+const inline = document.getElementById('inline')
 
 submit.addEventListener('click',(e)=>{
     e.preventDefault();
     var login = document.getElementById('login')
     var senha = document.getElementById('senha')
     
-    alert(login.value)
-    alert(senha.value)
-})
-
-
-db.collection('usuarios').get().then((doc)=>{
-    doc.forEach((doc)=>{
-        
+    db.collection('usuarios').add({
+        login: login.value,
+        senha: senha.value
+    }).then(()=>{
+        alert('usuário cadastrado com sucesso')
+        login.value = ''
+        senha.value = ''
+    })
+    db.collection('usuarios').get().then((doc)=>{
+        doc.forEach((doc)=>{
+                renderDoc(doc)
+        })
     })
 })
+
+function renderDoc(doc){
+    var login = doc.data().login;
+    var senha = doc.data().senha;
+
+    var loginHTML = document.createElement('p')
+    loginHTML.style.paddingLeft = "12%";
+    loginHTML.style.paddingRight = "12%";
+    loginHTML.style.display="inline"
+    loginHTML.innerText = login;
+    var senhaHTML = document.createElement('p')
+    senhaHTML.style.paddingLeft = "12%";
+    senhaHTML.style.paddingRight = "12%";
+    senhaHTML.style.display="inline"
+    senhaHTML.innerText = senha;
+    inline.after(document.createElement('div'))
+    inline.after(senhaHTML)
+    inline.after(loginHTML)
+}
+
+
